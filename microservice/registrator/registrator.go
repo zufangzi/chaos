335ac73ae80d3eabd890446b696888b036cb3966
surlymo <surlymo@126.com>
2016-03-10 11:43:08 +0800

    microservice base update

--- microservice/registrator/registrator.go
+++ microservice/registrator/registrator.go
@@ -1,48 +1,36 @@
 package main
 
 import (
-	"errors"
 	"flag"
 	// "fmt"
 	consulApi "github.com/hashicorp/consul/api"
 	"github.com/hashicorp/go-cleanhttp"
 	"log"
+	"opensource/chaos/microservice/utils"
 	"os"
-	"os/exec"
 	"strconv"
-	"strings"
 	"time"
 )
 
 var consulClient *consulApi.Client
+var signFile string
 var serviceId, serviceName, servicePort, serviceIp string
 var serviceTags []string
 var isNormal bool
 var fatalExit chan int
 var monitorInterval = flag.Int("monitor-interval", 2, "Interval(in millisecond) between monitor attemps")
 
-func assert(param interface{}) {
-	if param == nil {
-		os.Exit(2)
-	}
-	switch param.(type) {
-	case string:
-		if param == "" {
-			os.Exit(2)
-		}
-	}
-}
-
 func initEnv() {
 	// 让命令行的命令生效
 	flag.Parse()
 	// 初始化一些初始变量
-	serviceIp = getShell("hostname -i")
+	serviceIp = utils.GetShell("hostname -i")
 	serviceName = os.Getenv("SERVICE_NAME")
-	assert(serviceName)
+	utils.Assert(serviceName)
 	servicePort = os.Getenv("SERVICE_PORT")
-	assert(servicePort)
+	utils.Assert(servicePort)
 	serviceId = strconv.FormatInt(time.Now().Unix(), 10) + "_" + serviceName
+	signFile = "/home/work/kickoff_sign_file"
 	isNormal = false
 
 	// 给一个channel，如果没有严重错误就永久等待
@@ -51,7 +39,9 @@ func initEnv() {
 
 func defaultConfig() *consulApi.Config {
 	// 拿ip最后一位设置为1，即为宿主机ip。默认宿主机上必须有consul
-	hostIp := serviceIp[:strings.LastIndex(serviceIp, ".")] + ".1:5000"
+	// Deprecated。采用OVS+none划分VLAN的方式重做二层网络。采用读取信号量文件来拿
+	// hostIp := serviceIp[:strings.LastIndex(serviceIp, ".")] + ".1:5000"
+	hostIp := utils.GetShell("cat " + signFile + " | head -n1")
 	config := &consulApi.Config{
 		Address:    hostIp,
 		Scheme:     "http",
@@ -74,7 +64,7 @@ func main() {
 	// ticker := time.NewTicker(time.Duration(*monitorInterval) * time.Millisecond)
 	log.Println("[REGISTRATOR]Now begin to listen the port...")
 	for {
-		if !monitor() { // 说明启动还未成功
+		if !utils.PortExists(servicePort) { // 说明启动还未成功
 			log.Println("[REGISTRATOR]Not found port for registration, please wait...")
 			time.Sleep(time.Duration(*monitorInterval) * time.Millisecond)
 			continue
@@ -99,7 +89,7 @@ func main() {
 			select {
 			case <-ticker.C:
 				log.Println("[REGISTRATOR]Now in a loop monitor ticket time!")
-				if monitor() {
+				if utils.PortExists(servicePort) {
 					if !isNormal {
 						log.Println("[REGISTRATOR]Now begin to register!")
 						isNormal = true
@@ -117,23 +107,6 @@ func main() {
 	}()
 
 	log.Fatalln("[REGISTRATOR]Error...", <-fatalExit)
-}
-
-func getShell(cmdStr string) string {
-	cmd := exec.Command("/bin/sh", "-c", cmdStr)
-	out, err := cmd.Output()
-	if err != nil {
-		panic(errors.New("error occur while process shell cmd"))
-	}
-	cleanOut := string(out)
-	realOut := cleanOut[0:strings.Index(cleanOut, "\n")]
-	return realOut
-}
-
-func monitor() bool {
-	cnt, _ := strconv.Atoi(getShell("netstat -nlp | grep \":" + servicePort + "\"|wc -l"))
-	log.Println("[REGISTRATOR]Now found counts: ", cnt)
-	return cnt > 0
 }
 
 func register() {
---
+++ microservice/utils/shells.go
@@ -0,0 +1,29 @@
+package utils
+
+import (
+	"errors"
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+func GetShell(cmdStr string) string {
+	cmd := exec.Command("/bin/sh", "-c", cmdStr)
+	out, err := cmd.Output()
+	if err != nil {
+		panic(errors.New("error occur while process shell cmd"))
+	}
+	cleanOut := string(out)
+	realOut := cleanOut[0:strings.Index(cleanOut, "\n")]
+	return realOut
+}
+
+func PortExists(servicePort string) bool {
+	cnt, _ := strconv.Atoi(GetShell("netstat -nlp | grep \":" + servicePort + "\"|wc -l"))
+	return cnt > 0
+}
+
+func FileExists(signPath, signFile string) bool {
+	cnt, _ := strconv.Atoi(GetShell("find " + signPath + " -name " + signFile + " | wc -l"))
+	return cnt > 0
+}
